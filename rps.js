dad81a394b7d441007511f6f6ff4b22501d4dcc9
Joosh <jkrowden@gmail.com>
2022-02-24 09:54:30 -0700

    chat and menu coming together html

--- rps.js
+++ rps.js
@@ -1,4 +1,8 @@
+
+
 $(document).ready(() => {
-    $("#ties").append("content");
-    $("#chatlog").val("hello world!");
+    $("#ties").append("content069");
+    for(var i = 0; i < 12; ++i){
+        $("#chatlog").val(i + "//Hello Worlddd!!;\n" + $("#chatlog").val())
+    }
 });
