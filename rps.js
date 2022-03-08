9a716a20e4611890c6253e58f21b32f9456fcf83
jkreal <jkrowden@gmail.com>
2022-03-08 07:35:32 -0700

    made resize required for portrait mobile

--- rps.js
+++ rps.js
@@ -1,8 +1,34 @@
+var entirety;
+var resizeNeeded = false;
 
+function emptyBox() {
+  entirety = $("#outside-box").html();
+  resizeNeeded = true;
+  $("#outside-box").empty();
+}
 
 $(document).ready(() => {
-    $("#ties").append("content069");
-    for(var i = 0; i < 12; ++i){
-        $("#chatlog").val(i + "//Hello Worlddd!!;\n" + $("#chatlog").val())
+  $("#ties").append("content069");
+  for (var i = 0; i < 12; ++i) {
+    $("#chatlog").val(i + "//Hello Worlddd!!;\n" + $("#chatlog").val());
+  }
+
+  if ($(window).width() < 420) {
+    emptyBox();
+  }
+});
+
+$(window).resize(function () {
+  setTimeout(() => {
+    if (resizeNeeded === true) {
+      if ($(window).width() > 420) {
+        $("#outside-box").html(entirety);
+        resizeNeeded = false;
+      }
+    } else {
+        if ($(window).width() <= 420) {
+            emptyBox();
+        }
     }
+  }, 50);
 });
