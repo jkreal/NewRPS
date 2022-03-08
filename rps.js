060894c447148baba622d530a9c436842eb633f5
jkreal <jkrowden@gmail.com>
2022-03-08 08:03:20 -0700

    more progress on media queries

--- rps.js
+++ rps.js
@@ -19,7 +19,10 @@ $(document).ready(() => {
 });
 
 $(window).resize(function () {
+    // timeout needed to delay check for window width
   setTimeout(() => {
+    //   This if block empties the container if on mobile (portrait)
+    //   Also is responsible for returning content when at correct width
     if (resizeNeeded === true) {
       if ($(window).width() > 420) {
         $("#outside-box").html(entirety);
