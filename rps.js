dc389b0ecdaf06799fe0083864b14b9f3fa59df5
jkreal <jkrowden@gmail.com>
2022-03-31 12:19:57 -0600

    redesigning for mobile first

--- rps.js
+++ rps.js
@@ -5,33 +5,29 @@ function emptyBox() {
   entirety = $("#outside-box").html();
   resizeNeeded = true;
   $("#outside-box").empty();
+  $("#outside-box").html(
+    "<h1>This game does not work in portrait mode! Please turn your phone over!</h1>"
+  );
 }
 
 $(document).ready(() => {
-  $("#ties").append("content069");
-  for (var i = 0; i < 12; ++i) {
-    $("#chatlog").val(i + "//Hello Worlddd!!;\n" + $("#chatlog").val());
-  }
-
   if ($(window).width() < 420) {
     emptyBox();
   }
 });
 
 $(window).resize(function () {
-    // timeout needed to delay check for window width
+  // timeout needed to delay check for window width
   setTimeout(() => {
     //   This if block empties the container if on mobile (portrait)
     //   Also is responsible for returning content when at correct width
-    if (resizeNeeded === true) {
-      if ($(window).width() > 420) {
-        $("#outside-box").html(entirety);
-        resizeNeeded = false;
-      }
+    if (resizeNeeded === true && $(window).width() > 420) {
+      $("#outside-box").html(entirety);
+      resizeNeeded = false;
     } else {
-        if ($(window).width() <= 420) {
-            emptyBox();
-        }
+      if ($(window).width() <= 420) {
+        emptyBox();
+      }
     }
   }, 50);
 });
