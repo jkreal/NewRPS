1383e4034e1c12983f6c661466fbc3750e1d9b06
Joosh <jkrowden@gmail.com>
2022-04-02 05:58:41 -0600

    Added basic message functionality

--- rps.js
+++ rps.js
@@ -1,6 +1,8 @@
 var entirety;
 var resizeNeeded = false;
+var messageLog = "";
 
+// This function is used to empty the contents of the game in case it detects the user is using a phone in portrait mode.
 function emptyBox() {
   entirety = $("#outside-box").html();
   resizeNeeded = true;
@@ -10,14 +12,78 @@ function emptyBox() {
   );
 }
 
+function addMessage(message) {
+  messageLog = message + "\n" + messageLog;
+  $("#main-text").val(messageLog);
+}
+
 $(document).ready(() => {
   if ($(window).width() < 420) {
     emptyBox();
   }
-
-  $("#main-text").append("Welcome to Rock Paper Scissors!");
-  $("#main-text").append("Welcome to Rock Paper Scissors!2");
+  addMessage(">Welcome to Rock Paper Scissors!");
+  addMessage(">To begin, select your choice!");
 });
+
+$(".image").on("click", function () {
+  addMessage("You chose " + $(this).attr("alt") + ".");
+});
+
+function rockPaperScissors(choice) {
+  var compChoice = Math.floor(Math.random() * 3);
+  switch (choice) {
+    //   Player chose Rock
+    case 0:
+      switch (compChoice) {
+        //   Tie
+        case 0:
+          break;
+        //   Lose
+        case 1:
+          break;
+        //   Win
+        case 2:
+          break;
+        default:
+          break;
+      }
+      break;
+    //   Player chose paper
+    case 1:
+      switch (compChoice) {
+        //   Win
+        case 0:
+          break;
+        //   Tie
+        case 1:
+          break;
+        //   Lose
+        case 2:
+          break;
+        default:
+          break;
+      }
+      break;
+    //   Player chose scissors
+    case 2:
+      switch (compChoice) {
+        //   Lose
+        case 0:
+          break;
+        //   Win
+        case 1:
+          break;
+        //   Tie
+        case 2:
+          break;
+        default:
+          break;
+      }
+      break;
+    default:
+      break;
+  }
+}
 
 $(window).resize(function () {
   // timeout needed to delay check for window width
